6599dffb0d72c661e8a8ca513bc0786c9c1c831e
Joseph McDonough <joseph.mcdonough1@marist.edu>
2021-09-15 21:06:59 -0400

    Adding the necessary files and the statements outlined in the hints section of iProject2

--- distrib/os/kernel.js
+++ distrib/os/kernel.js
@@ -32,6 +32,8 @@ var TSOS;
             //
             // ... more?
             //
+            // Memory
+            //_MemoryManager = new MemoryManager();
             // Enable the OS Interrupts.  (Not the CPU clock interrupt, as that is done in the hardware sim.)
             this.krnTrace("Enabling the interrupts.");
             this.krnEnableInterrupts();
